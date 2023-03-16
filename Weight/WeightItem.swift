e10a326a6a5cd4524d74a779feda5c9f0c4f22dc
John Gers <johnygers@gmail.com>
2023-03-16 14:49:05 -0400

    Create Weight framework

---
+++ Weight/WeightItem.swift
@@ -0,0 +1,14 @@
+//
+//  WeightItem.swift
+//  Weight
+//
+//  Created by John Gers on 3/16/23.
+//
+
+import Foundation
+
+struct WeightItem {
+    let id: UUID
+    let weight: Double
+    let date: Date
+}
