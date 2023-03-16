8b33166d994b0751b774b154c3efde42a6844f5d
John Gers <johnygers@gmail.com>
2023-03-16 16:33:59 -0400

    Save command requests new cache insertion on successful insertion

--- WeightTests/CacheWeightLogUseCase.swift
+++ WeightTests/CacheWeightLogUseCase.swift
@@ -16,20 +16,35 @@ class LocalWeightLogLoader {
     }
     
     func save(_ items: [WeightItem]) {
-        store.save(items)
+        store.save(items) { [unowned self] error in
+            if error == nil {
+                self.store.insert(items)
+            }
+        }
     }
 }
 
 class WeightLogStore {
+    typealias InsertionCompletion = (Error?) -> Void
     var saveCallCount = 0
     var insertCallCount = 0
     
-    func save(_ items: [WeightItem]) {
+    private var insertionCompletions: [InsertionCompletion] = []
+    func save(_ items: [WeightItem], completion: @escaping InsertionCompletion) {
         saveCallCount += 1
+        insertionCompletions.append(completion)
     }
     
     func completeInsertion(with error: Error, at index: Int = 0) {
-        
+        insertionCompletions[index](error)
+    }
+    
+    func completeInsertionSuccessfully(at index: Int = 0) {
+        insertionCompletions[index](nil)
+    }
+    
+    func insert(_ items: [WeightItem]) {
+        insertCallCount += 1
     }
 }
 
@@ -52,6 +67,16 @@ class CacheWeightLogUseCaseTests: XCTestCase {
         store.completeInsertion(with: saveError)
         
         XCTAssertEqual(store.insertCallCount, 0)
+    }
+    
+    func test_save_requestsNewCacheInsertionOnSuccessfulInsertion() {
+        let items = [uniqueItem()]
+        let (sut, store) = makeSUT()
+
+        sut.save(items)
+        store.completeInsertionSuccessfully()
+        
+        XCTAssertEqual(store.insertCallCount, 1)
     }
     
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: LocalWeightLogLoader, store: WeightLogStore) {
