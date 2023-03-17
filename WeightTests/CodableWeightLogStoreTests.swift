69eec7152ab27e0a02bd7425975acd9a00345216
John Gers <johnygers@gmail.com>
2023-03-17 14:17:05 -0400

    Extract reusable WeightLogStoreSpecs helper methods into a shared scope so it can be used by there WeightLogStore implementation tests

--- WeightTests/CodableWeightLogStoreTests.swift
+++ WeightTests/CodableWeightLogStoreTests.swift
@@ -205,65 +205,6 @@ class CodableWeightLogStoreTests: XCTestCase, FailableWeightLogStoreSpecs {
         return sut
     }
     
-    @discardableResult
-    private func save(_ cache: [LocalWeightItem], to sut: WeightLogStore, file: StaticString = #file, line: UInt = #line) -> Error? {
-        let exp = expectation(description: "Wait for cache retrieval")
-        
-        var saveError: Error?
-        sut.save(cache) { retrievedSaveError  in
-            saveError = retrievedSaveError
-            exp.fulfill()
-        }
-        
-        wait(for: [exp], timeout: 1.0)
-        return saveError
-    }
-    
-    @discardableResult
-    private func deleteCache(from sut: WeightLogStore) -> Error? {
-        let exp = expectation(description: "Wait for cache deletion")
-        var deletionError: Error?
-        sut.deleteCachedLog { deletionResult in
-            switch deletionResult {
-            case .success:
-                deletionError = nil
-            case let .failure(error):
-                deletionError = error
-            }
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: 5.0)
-        return deletionError
-    }
-    
-    private func expect(_ sut: WeightLogStore, toRetrieveTwice expectedResult: RetrieveCachedLogResult, file: StaticString = #file, line: UInt = #line) {
-        expect(sut, toRetrieve: expectedResult, file: file, line: line)
-        expect(sut, toRetrieve: expectedResult, file: file, line: line)
-    }
-
-    private func expect(_ sut: WeightLogStore, toRetrieve expectedResult: RetrieveCachedLogResult, file: StaticString = #file, line: UInt = #line) {
-        let exp = expectation(description: "Wait for cache retrieval")
-        
-        sut.retrieve { retrievedResult in
-            switch (expectedResult, retrievedResult) {
-            case (.empty, .empty),
-                (.failure, .failure):
-                break
-                
-            case let (.found(expected), .found(retrieved)):
-                XCTAssertEqual(retrieved, expected, file: file, line: line)
-                
-            default:
-                XCTFail("Expected to retrieve \(expectedResult), got \(retrievedResult) instead", file: file, line: line)
-            }
-            
-            exp.fulfill()
-        }
-        
-        wait(for: [exp], timeout: 1.0)
-    }
-
-    
     private func setupEmptyStoreState() {
         deleteStoreArtifacts()
     }
