3da512d785108eab776203e6956e0a2d1e6fd75f
John Gers <johnygers@gmail.com>
2023-03-17 12:59:51 -0400

    Proved that the CodableWeightLogStore side-effects run serially

--- WeightTests/CodableWeightLogStoreTests.swift
+++ WeightTests/CodableWeightLogStoreTests.swift
@@ -202,6 +202,33 @@ class CodableWeightLogStoreTests: XCTestCase {
         expect(sut, toRetrieve: .empty)
     }
     
+    func test_storeSideEffect_runSerially() {
+        let sut = makeSUT()
+        var completedOperationsInOrder: [XCTestExpectation] = []
+        
+        let op1 = expectation(description: "Operation 1")
+        sut.save(uniqueWeightLog().local) { _ in
+            completedOperationsInOrder.append(op1)
+            op1.fulfill()
+        }
+        
+        let op2 = expectation(description: "Operation 2")
+        sut.deleteCachedLog { _ in
+            completedOperationsInOrder.append(op2)
+            op2.fulfill()
+        }
+        
+        let op3 = expectation(description: "Operation 3")
+        sut.save(uniqueWeightLog().local) { _ in
+            completedOperationsInOrder.append(op3)
+            op3.fulfill()
+        }
+        
+        waitForExpectations(timeout: 5.0)
+        
+        XCTAssertEqual(completedOperationsInOrder, [op1, op2, op3], "Expected side-effects to run serially but operations finished in the wrong order")
+    }
+    
     // - MARK: Helpers
     
     private func makeSUT(storeURL: URL? = nil, file: StaticString = #file, line: UInt = #line) -> CodableWeightLogStore {
