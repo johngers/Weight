79378c7e141011e8d4674f62c0a01fb4330dc94e
John Gers <johnygers@gmail.com>
2023-03-17 17:55:38 -0400

    Add WeightCacheIntegrationTest to separate the potentially-slower cache integration tests from the fast unit/isolated tests

---
+++ WeightTests/WeightCacheIntegrationTests.swift
@@ -0,0 +1,13 @@
+//
+//  WeightCacheIntegrationTests.swift
+//  WeightTests
+//
+//  Created by John Gers on 3/17/23.
+//
+
+import XCTest
+import Weight
+
+class WeightCacheIntegrationTests: XCTestCase {
+    
+}
