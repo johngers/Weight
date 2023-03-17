31f7f8690d176a5df45547dd037242dc76029369
John Gers <johnygers@gmail.com>
2023-03-17 01:07:52 -0400

    Add helper methods to provide documentation context and clarify test setUp and tearDown intents

--- WeightTests/CodableWeightLogStoreTests.swift
+++ WeightTests/CodableWeightLogStoreTests.swift
@@ -63,13 +63,13 @@ class CodableWeightLogStoreTests: XCTestCase {
     override func setUp() {
         super.setUp()
         
-        try? FileManager.default.removeItem(at: testSpecificStoreURL())
+        setupEmptyStoreState()
     }
-
+    
     override func tearDown() {
         super.tearDown()
         
-        try? FileManager.default.removeItem(at: testSpecificStoreURL())
+        undoStoreSideEffects()
     }
 
     func test_retrieve_deliversEmptyOnEmptyCache() {
@@ -137,6 +137,18 @@ class CodableWeightLogStoreTests: XCTestCase {
         let sut = CodableWeightLogStore(storeURL: testSpecificStoreURL())
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
+    }
+    
+    private func setupEmptyStoreState() {
+        deleteStoreArtifacts()
+    }
+    
+    private func undoStoreSideEffects() {
+        deleteStoreArtifacts()
+    }
+    
+    private func deleteStoreArtifacts() {
+        try? FileManager.default.removeItem(at: testSpecificStoreURL())
     }
     
     private func testSpecificStoreURL() -> URL {
