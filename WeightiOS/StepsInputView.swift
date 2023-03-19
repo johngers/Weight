3f8620bcbdb40509aec0163087d9b407d6141b7e
John Gers <johnygers@gmail.com>
2023-03-19 18:37:59 -0400

    Create StepsInputView which will display steps progress easily at a glance

---
+++ WeightiOS/StepsInputView.swift
@@ -0,0 +1,37 @@
+//
+//  StepsInputView.swift
+//  WeightiOS
+//
+//  Created by John Gers on 3/19/23.
+//
+
+import SwiftUI
+
+struct StepsInputView: View {
+
+    var body: some View {
+        VStack {
+            TabHeaderView(imageName: "figure.walk", lastUpdated: "Last updated: March 19th", title: "Statistics", tabTitle: "Steps", units: [.miles, .kilometer], color: .systemPurple, settingsSelection: { })
+            
+            Spacer()
+                .frame(height: 25)
+            
+            Spacer()
+
+            CurrentProgressView(current: "1000", goal: "10000", color: .purple.opacity(0.5))
+            
+            Spacer()
+            
+            ChartCardView(color: .purple.opacity(0.5))
+                .frame(height: 150)
+        }
+        .padding()
+    }
+}
+
+struct StepsInputView_Previews: PreviewProvider {
+    static var previews: some View {
+        StepsInputView()
+    }
+}
+
