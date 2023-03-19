972cf4ce414c656eb98288c118320aa7ef4b5932
John Gers <johnygers@gmail.com>
2023-03-18 20:52:47 -0400

    Create mock data to build a chart view

--- Weight/WeightItem.swift
+++ Weight/WeightItem.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public struct WeightItem: Equatable {
+public struct WeightItem: Equatable, Identifiable {
     public let id: UUID
     public let weight: Double
     public let date: Date
@@ -16,5 +16,47 @@ public struct WeightItem: Equatable {
         self.id = id
         self.weight = weight
         self.date = date
+    }
+}
+
+public struct ChartState {
+    public let weightData: [WeightItem] = [
+        .init(id: .init(), weight: 100.0, date: .now.adding(days: -30)),
+        .init(id: .init(), weight: 101.0, date: .now.adding(days: -29)),
+        .init(id: .init(), weight: 102.0, date: .now.adding(days: -28)),
+        .init(id: .init(), weight: 103.0, date: .now.adding(days: -27)),
+        .init(id: .init(), weight: 104.0, date: .now.adding(days: -26)),
+        .init(id: .init(), weight: 105.0, date: .now.adding(days: -25)),
+        .init(id: .init(), weight: 105.0, date: .now.adding(days: -24)),
+        .init(id: .init(), weight: 106.0, date: .now.adding(days: -23)),
+        .init(id: .init(), weight: 107.0, date: .now.adding(days: -22)),
+        .init(id: .init(), weight: 108.0, date: .now.adding(days: -21)),
+        .init(id: .init(), weight: 109.0, date: .now.adding(days: -20)),
+        .init(id: .init(), weight: 110.0, date: .now.adding(days: -19)),
+        .init(id: .init(), weight: 111.0, date: .now.adding(days: -18)),
+        .init(id: .init(), weight: 112.0, date: .now.adding(days: -17)),
+        .init(id: .init(), weight: 113.0, date: .now.adding(days: -16)),
+        .init(id: .init(), weight: 114.0, date: .now.adding(days: -15)),
+        .init(id: .init(), weight: 115.0, date: .now.adding(days: -14)),
+        .init(id: .init(), weight: 116.0, date: .now.adding(days: -13)),
+        .init(id: .init(), weight: 117.0, date: .now.adding(days: -12)),
+        .init(id: .init(), weight: 118.0, date: .now.adding(days: -11)),
+        .init(id: .init(), weight: 119.0, date: .now.adding(days: -10)),
+        .init(id: .init(), weight: 120.0, date: .now.adding(days: -9)),
+        .init(id: .init(), weight: 121.0, date: .now.adding(days: -8)),
+        .init(id: .init(), weight: 122.0, date: .now.adding(days: -7)),
+        .init(id: .init(), weight: 123.0, date: .now.adding(days: -6)),
+        .init(id: .init(), weight: 124.0, date: .now.adding(days: -5)),
+        .init(id: .init(), weight: 125.0, date: .now.adding(days: -4)),
+        .init(id: .init(), weight: 126.0, date: .now.adding(days: -3)),
+        .init(id: .init(), weight: 127.0, date: .now.adding(days: -2)),
+        .init(id: .init(), weight: 128.0, date: .now.adding(days: -1)),
+        .init(id: .init(), weight: 129.0, date: .now),
+    ]
+    
+    public let sortedData: [WeightItem]
+    
+    public init() {
+        sortedData = weightData.sorted(by: { $0.weight < $1.weight })
     }
 }
