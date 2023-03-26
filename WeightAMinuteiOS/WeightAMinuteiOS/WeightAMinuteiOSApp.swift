2a4281b1930dd6c084793ac749f6adc34236b0c4
John Gers <johnygers@gmail.com>
2023-03-25 20:20:35 -0400

    Create iOS app project

---
+++ WeightAMinuteiOS/WeightAMinuteiOS/WeightAMinuteiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeightAMinuteiOSApp.swift
+//  WeightAMinuteiOS
+//
+//  Created by John Gers on 3/25/23.
+//
+
+import SwiftUI
+
+@main
+struct WeightAMinuteiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("Hi")
+        }
+    }
+}
