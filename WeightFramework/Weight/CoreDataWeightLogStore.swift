8d8aa876d209fbba852a1411a965f777db4387cb
John Gers <johnygers@gmail.com>
2023-03-28 22:24:12 -0400

    Move WeightLogStore conformance for the CoreDataWeightLogStore to an extension

--- WeightFramework/Weight/CoreDataWeightLogStore.swift
+++ WeightFramework/Weight/CoreDataWeightLogStore.swift
@@ -7,7 +7,7 @@
 
 import CoreData
 
-public final class CoreDataWeightLogStore: WeightLogStore {
+public final class CoreDataWeightLogStore {
     private let container: NSPersistentContainer
     private let context: NSManagedObjectContext
     
@@ -16,36 +16,7 @@ public final class CoreDataWeightLogStore: WeightLogStore {
         context = container.newBackgroundContext()
     }
     
-    public func retrieve(completion: @escaping RetrievalCompletion) {
-        perform { context in
-            completion(Result{
-                try ManagedCache.find(in: context).map {
-                    return CachedLog($0.localLog)
-                }
-            })
-        }
-    }
-    
-    public func save(_ log: [LocalWeightItem], completion: @escaping SaveCompletion) {
-        perform { context in
-            completion(Result{
-                let managedCache = try ManagedCache.currentInstance(in: context)
-                managedCache.log = ManagedWeightItem.items(from: log, in: context)
-                
-                try context.save()
-            })
-        }
-    }
-    
-    public func deleteCachedLog(completion: @escaping DeletionCompletion) {
-        perform { context in
-            completion(Result{
-                try ManagedCache.find(in: context).map(context.delete).map(context.save)
-            })
-        }
-    }
-    
-    private func perform(_ action: @escaping (NSManagedObjectContext) -> Void) {
+    func perform(_ action: @escaping (NSManagedObjectContext) -> Void) {
         let context = self.context
         context.perform { action(context) }
     }
